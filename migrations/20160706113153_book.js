8077fb580eacdbb8262f8bf8fb65142ba24fdde5
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-06 12:53:02 -0600

    initial commit, express generated, knex, bookshelf

---
+++ migrations/20160706113153_book.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('book', function(table) {
+    table.increments();
+    table.string('title');
+    table.string('description');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('book');
+};
